b86962348c02b0a2db8757baa7ed88a37c1bab74
Erik Grundy <erik.grundy@gmail.com>
2023-12-08 08:55:22 +1300

    tidy and optimise day 7 (specifically part 2)

--- lib/Day07.hs
+++ lib/Day07.hs
@@ -4,63 +4,18 @@ module Day07 (solve, parse, Card (..), Hand (..), HandType, part1, part2) where
 
 import Control.Applicative (asum)
 import Control.Arrow ((&&&))
-import Data.Function (on)
+import Data.Function (on, (&))
 import Data.Functor (($>), (<&>))
-import Data.List (delete, group, sort, sortOn)
+import Data.List (group, maximumBy, partition, sort, sortOn)
+import Data.Ord (comparing)
 import qualified Text.Parsec as P
 
-data Card
-  = Two
-  | Three
-  | Four
-  | Five
-  | Six
-  | Seven
-  | Eight
-  | Nine
-  | T
-  | J
-  | Q
-  | K
-  | A
-  deriving (Show, Eq, Ord, Enum)
-
-newtype Hand = Hand {cards :: [Card]} deriving (Eq, Show)
-
-instance Ord Hand where
-  compare = (compare `on` handType) `thenOn` cards
-
-data HandType
-  = OneOfAKind
-  | OnePair
-  | TwoPair
-  | ThreeOfAKind
-  | FullHouse
-  | FourOfAKind
-  | FiveOfAKind
-  deriving (Show, Eq, Ord)
+-- common
 
 solve :: String -> (String, String)
 solve = ((display . fmap part1) &&& (display . fmap part2)) . parse
   where
     display = either (const "parse error") show
-
-handType :: Hand -> HandType
-handType = go . sort . map length . group . sort . cards
-  where
-    go [5] = FiveOfAKind
-    go [1, 4] = FourOfAKind
-    go [2, 3] = FullHouse
-    go [1, 1, 3] = ThreeOfAKind
-    go [1, 2, 2] = TwoPair
-    go [1, 1, 1, 2] = OnePair
-    go [1, 1, 1, 1, 1] = OneOfAKind
-    go _ = error "invalid hand"
-
-thenOn :: (Ord b) => (a -> a -> Ordering) -> (a -> b) -> a -> a -> Ordering
-thenOn comp mapping a1 a2 = case comp a1 a2 of
-  EQ -> (compare `on` mapping) a1 a2
-  notEq -> notEq
 
 parse :: String -> Either P.ParseError [(Hand, Int)]
 parse = P.parse parser ""
@@ -87,34 +42,58 @@ parse = P.parse parser ""
       ]
     intP = P.many P.digit <&> read
 
+-- part 1: without jokers
+
+data Card
+  = Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | T
+  | J
+  | Q
+  | K
+  | A
+  deriving (Show, Eq, Ord, Enum)
+
+newtype Hand = Hand {cards :: [Card]} deriving (Eq, Show)
+
+instance Ord Hand where
+  compare = compare `on` (handType &&& cards)
+
+data HandType
+  = OneOfAKind
+  | OnePair
+  | TwoPair
+  | ThreeOfAKind
+  | FullHouse
+  | FourOfAKind
+  | FiveOfAKind
+  deriving (Show, Eq, Ord)
+
+handType :: Hand -> HandType
+handType = go . sort . map length . group . sort . cards
+  where
+    go [5] = FiveOfAKind
+    go [1, 4] = FourOfAKind
+    go [2, 3] = FullHouse
+    go [1, 1, 3] = ThreeOfAKind
+    go [1, 2, 2] = TwoPair
+    go [1, 1, 1, 2] = OnePair
+    go [1, 1, 1, 1, 1] = OneOfAKind
+    go _ = error "invalid hand"
+
 part1 :: [(Hand, Int)] -> Int
 part1 = sum . zipWith winnings [1 ..] . sortOn fst
 
 winnings :: Int -> (a, Int) -> Int
 winnings rank (_, bet) = bet * rank
 
-part2 :: [(Hand, Int)] -> Int
-part2 = sum . zipWith winnings [1 ..] . sortOn (handWithJokers . fst)
-
-newtype HandWithJokers = HandWithJokers
-  { cardsWithJokers :: [CardWithJokers]
-  }
-  deriving (Show, Eq)
-
-instance Ord HandWithJokers where
-  compare = (compare `on` handTypeWithJokers) `thenOn` cardsWithJokers
-
-handWithJokers :: Hand -> HandWithJokers
-handWithJokers = HandWithJokers . map CardWithJokers . cards
-
-handTypeWithJokers :: HandWithJokers -> HandType
-handTypeWithJokers = maximum . map (handType . Hand) . evaluateHands . cardsWithJokers
-
-evaluateHands :: [CardWithJokers] -> [[Card]]
-evaluateHands [] = [[]]
-evaluateHands ((CardWithJokers J) : rest) =
-  (:) <$> delete J [Two .. A] <*> evaluateHands rest
-evaluateHands ((card : rest)) = map (getCard card :) (evaluateHands rest)
+-- part 2: handling jokers
 
 newtype CardWithJokers = CardWithJokers {getCard :: Card} deriving (Show, Eq)
 
@@ -124,3 +103,31 @@ instance Ord CardWithJokers where
     (_, J) -> GT
     (J, _) -> LT
     (x, y) -> compare x y
+
+newtype HandWithJokers = HandWithJokers
+  { cardsWithJokers :: [CardWithJokers]
+  }
+  deriving (Show, Eq)
+
+instance Ord HandWithJokers where
+  compare = compare `on` (handTypeWithJokers &&& cardsWithJokers)
+
+handTypeWithJokers :: HandWithJokers -> HandType
+handTypeWithJokers (HandWithJokers hand) =
+  maybe hand ((<> others) . replicate (length jokers)) (mode others)
+    & map getCard
+    & Hand
+    & handType
+  where
+    (jokers, others) = partition ((== J) . getCard) hand
+
+mode :: (Ord a) => [a] -> Maybe a
+mode [] = Nothing
+mode elements =
+  elements & sort & group & maximumBy (comparing length) & head & Just
+
+handWithJokers :: Hand -> HandWithJokers
+handWithJokers = HandWithJokers . map CardWithJokers . cards
+
+part2 :: [(Hand, Int)] -> Int
+part2 = sum . zipWith winnings [1 ..] . sortOn (handWithJokers . fst)
