5622696c0b4df4e7b8e828dadb38a183d4c7e1ca
Erik Grundy <erik.grundy@gmail.com>
2023-12-13 00:28:22 +1300

    simplify: handle start once and then not again

--- lib/Day10.hs
+++ lib/Day10.hs
@@ -1,7 +1,7 @@
 module Day10 (part1, part2, solve, parse) where
 
 import Control.Arrow ((&&&))
-import Data.Array (Array, Ix (inRange), assocs, bounds, listArray, (!))
+import Data.Array (Array, Ix (inRange), assocs, bounds, listArray, (!), (//))
 import Data.Bifunctor (Bifunctor (bimap))
 import Data.Foldable (Foldable (toList))
 import Data.Function ((&))
@@ -18,26 +18,12 @@ type Input = (Map, Position)
 solve :: String -> (String, String)
 solve = ((show . part1) &&& (show . part2)) . parse
 
--- TODO: handle start by fixing its value in the grid, seeing as we already know
--- its starting position here. that way we can simplify logic below
+-- TODO: make a type for positions, instead of using characters
 parse :: String -> Input
-parse input = (grid, findStart grid)
-  where
-    grid = constructMap input
-
-part1 :: Input -> Int
-part1 (grid, start) = go 1 (n1, start) (n2, start)
-  where
-    (n1, n2) = neighboursOfStart grid start
-    next pos prev = case neighbours (grid ! pos) pos of
-      Right (next1, next2) -> (if next1 == prev then next2 else next1, pos)
-      Left err -> error err
-    go n (pos1, prev1) (pos2, prev2)
-      | pos1 == pos2 = n
-      | otherwise = go (n + 1) (next pos1 prev1) (next pos2 prev2)
-
-findStart :: Map -> Position
-findStart = fst . fromJust . find ((== 'S') . snd) . assocs
+parse input =
+  let grid = constructMap input
+      start = findStart grid
+   in (replaceStart grid start, start)
 
 constructMap :: String -> Map
 constructMap input = listArray ((0, 0), (r - 1, c - 1)) $ filter (/= '\n') input
@@ -45,30 +31,47 @@ constructMap input = listArray ((0, 0), (r - 1, c - 1)) $ filter (/= '\n') input
     r = length $ lines input
     c = length $ head $ lines input
 
-neighboursOfStart :: Map -> Position -> (Position, Position)
-neighboursOfStart grid pos =
-  case allNeighbours pos & filter (\n -> inGrid n && connectsToPos n) of
-    [n1, n2] -> (n1, n2)
-    _ -> error "start has the wrong number of connecting pipes"
+findStart :: Map -> Position
+findStart = fst . fromJust . find ((== 'S') . snd) . assocs
+
+replaceStart :: Map -> Position -> Map
+replaceStart grid start@(r, c) = grid // [(start, replacement)]
   where
+    replacement = case map isNeighbour allNeighbours of
+      [True, True, False, False] -> '|'
+      [False, False, False, False] -> '-'
+      [True, False, True, False] -> '7'
+      [True, False, False, True] -> 'F'
+      [False, True, True, False] -> 'J'
+      [False, True, False, True] -> 'L'
+      _ -> error "unreachable"
+    allNeighbours = [(r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1)]
+    isNeighbour n = inGrid n && (grid ! n) /= '.' && connectsToPos n
     inGrid = inRange (bounds grid)
-    connectsToPos neighbour = case neighbours (grid ! neighbour) neighbour of
-      Right (n1, n2) -> n1 == pos || n2 == pos
-      _ -> False
+    eitherIsStart (n1, n2) = n1 == start || n2 == start
+    connectsToPos n = eitherIsStart $ neighbours (grid ! n) n
 
-allNeighbours :: Position -> [Position]
-allNeighbours (r, c) = [(r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1)]
+part1 :: Input -> Int
+part1 (grid, start) = go 1 (n1, start) (n2, start)
+  where
+    (n1, n2) = neighbours (grid ! start) start
+    next pos prev =
+      let (next1, next2) = neighbours (grid ! pos) pos
+       in (if next1 == prev then next2 else next1, pos)
+    go n (pos1, prev1) (pos2, prev2)
+      | pos1 == pos2 = n
+      | otherwise = go (n + 1) (next pos1 prev1) (next pos2 prev2)
 
-neighbours :: Char -> Position -> Either String (Position, Position)
-neighbours '|' (r, c) = Right ((r - 1, c), (r + 1, c))
-neighbours '-' (r, c) = Right ((r, c - 1), (r, c + 1))
-neighbours 'L' (r, c) = Right ((r - 1, c), (r, c + 1))
-neighbours 'J' (r, c) = Right ((r - 1, c), (r, c - 1))
-neighbours '7' (r, c) = Right ((r + 1, c), (r, c - 1))
-neighbours 'F' (r, c) = Right ((r + 1, c), (r, c + 1))
-neighbours '.' pos = Left $ "unexpected ground at position " <> show pos
+neighbours :: Char -> Position -> (Position, Position)
+neighbours '|' (r, c) = ((r - 1, c), (r + 1, c))
+neighbours '-' (r, c) = ((r, c - 1), (r, c + 1))
+neighbours 'L' (r, c) = ((r - 1, c), (r, c + 1))
+neighbours 'J' (r, c) = ((r - 1, c), (r, c - 1))
+neighbours '7' (r, c) = ((r + 1, c), (r, c - 1))
+neighbours 'F' (r, c) = ((r + 1, c), (r, c + 1))
+neighbours '.' pos = error $ "unexpected ground at position " <> show pos
 neighbours c pos =
-  Left $ "unexpected char '" <> show c <> "' at position " <> show pos
+  error $ "unexpected char '" <> show c <> "' at position " <> show pos
 
 -- part 2
 
@@ -80,7 +83,7 @@ part2 :: Input -> Int
 part2 (grid, start) = areaOfLoop vertices - (length points `div` 2) + 1
   where
     points = getPoints grid start
-    vertices = filter (isCorner grid) points
+    vertices = filter ((`elem` "F7JL") . (grid !)) points
 
 -- the shoelace theorem: find the area of a space by representing it as the sum
 -- of signed areas of trapezoids. see here for more:
@@ -98,19 +101,10 @@ areaOfLoop vertices = [1 .. len] & map trapezoid & sum & (`div` 2) & abs
 getPoints :: Map -> Position -> [Position]
 getPoints grid start = NonEmpty.unfoldr f (False, start, second) & toList
   where
-    (_, second) = neighboursOfStart grid start
+    (_, second) = neighbours (grid ! start) start
     f (doneWithStart, current, next)
       | next == start && doneWithStart = (current, Nothing)
       | otherwise = (current, pure (True, next, next'))
       where
-        next' = either error select $ neighbours (grid ! next) next
+        next' = select $ neighbours (grid ! next) next
         select (n1, n2) = if n1 == current then n2 else n1
-
-isCorner :: Map -> Position -> Bool
-isCorner grid pos =
-  (grid ! pos) `elem` "F7JL" || ((grid ! pos) == 'S' && isStartCorner grid pos)
-
-isStartCorner :: Map -> Position -> Bool
-isStartCorner grid pos =
-  let ((r1, c1), (r2, c2)) = neighboursOfStart grid pos
-   in r1 /= r2 && c1 /= c2
