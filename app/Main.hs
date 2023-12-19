d107138748488a0150833d704398f95746921fb8
Erik Grundy <erik.grundy@gmail.com>
2023-12-20 00:11:18 +1300

    add day 13 to main

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@ import qualified Day09
 import qualified Day10
 import qualified Day11
 import qualified Day12
+import qualified Day13
 import Paths_aoc2023 (getDataFileName)
 import Text.Printf (printf)
 
@@ -38,5 +39,6 @@ days =
     (09, Day09.solve),
     (10, Day10.solve),
     (11, Day11.solve),
-    (12, Day12.solve)
+    (12, Day12.solve),
+    (13, Day13.solve)
   ]
