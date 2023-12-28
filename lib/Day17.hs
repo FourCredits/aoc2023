78bd6262b11bd94b6cd9aa220af6acd7c2fac4cd
Erik Grundy <erik.grundy@gmail.com>
2023-12-28 18:51:08 +1300

    tidy up and add to main

--- lib/Day17.hs
+++ lib/Day17.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ViewPatterns #-}
 
-module Day17 (parse, part1, part2) where
+module Day17 (parse, part1, part2, solve) where
 
 import Data.Array (Array, bounds, inRange, listArray, (!))
 import Data.Char (isDigit)
@@ -11,7 +11,7 @@ type Pos = (Int, Int)
 
 data Direction = L | R | U | D deriving (Eq, Ord, Show)
 
-data AStarState = AStarState
+data DayState = CrucibleState
   { position :: Pos,
     heatLoss :: Int,
     direction :: Direction,
@@ -19,8 +19,10 @@ data AStarState = AStarState
   }
   deriving (Eq, Show)
 
-instance Ord AStarState where
+instance Ord DayState where
   compare = compare `on` heatLoss
+
+type TurningRules = Int -> (DayState, DayState, DayState) -> [DayState]
 
 parse :: Text -> Maybe (Array Pos Int)
 parse input = do
@@ -30,78 +32,71 @@ parse input = do
   numbers <- traverse toNum $ filter isDigit $ toString input
   pure $ listArray ((0, 0), (rows - 1, columns - 1)) numbers
 
-part1 :: Array Pos Int -> Maybe Int
-part1 cityMap = go (insert start M.empty) M.empty
+solve :: Text -> (Text, Text)
+solve = (display part1 &&& display part2) . parse
   where
-    go (M.minView -> Just (current@(AStarState p hL _ _), rest)) visited
-      | p == end = pure hL
-      | isJust $ mfilter (<= hL) $ visited M.!? toKey current = go rest visited
-      | otherwise =
-          let priorityQueue' = foldr insert rest (options current)
-           in go priorityQueue' (M.insert (toKey current) hL visited)
-    go _ _ = empty
-    toKey (AStarState p _ d sl) = (p, d, sl)
-    insert = liftA2 M.insert (\(AStarState p hl d sl) -> (hl, p, d, sl)) id
-    start =
-      AStarState
-        { position = startPos,
-          heatLoss = 0,
-          direction = D,
-          straightLine = 0
-        }
-    cityLimits@(startPos, end) = bounds cityMap
-    options (AStarState pos loss dir straight) =
-      filter (inRange cityLimits . position) $ case straight of
-        3 -> [left, right]
-        _ -> [left, forward, right]
-      where
-        forward = newState dir straight
-        newState d sl =
-          let pos' = next pos d
-           in AStarState pos' (loss + (cityMap ! pos')) d (sl + 1)
-        (left, right) = case dir of
-          L -> (newState D 0, newState U 0)
-          R -> (newState U 0, newState D 0)
-          U -> (newState L 0, newState R 0)
-          D -> (newState R 0, newState L 0)
+    display f = maybe "parse error" (maybe "program error" show . f)
+
+part1 :: Array Pos Int -> Maybe Int
+part1 cityMap = aStar isFinished newOptions (mkStart cityMap)
+  where
+    isFinished (CrucibleState p _ _ _) = p == end
+    (_, end) = bounds cityMap
+    crucibleTurningRules n (left, forward, right)
+      | n >= 3 = [left, right]
+      | otherwise = [left, forward, right]
+    newOptions = mkNewOptions cityMap crucibleTurningRules
 
 part2 :: Array Pos Int -> Maybe Int
-part2 cityMap =
-  go (foldr insert M.empty [start, start {direction = R}]) M.empty
+part2 cityMap = aStar isFinished newOptions (mkStart cityMap)
   where
-    go (M.minView -> Just (current@(AStarState p hL _ sL), rest)) visited
-      | p == end && sL >= 4 = pure hL
+    isFinished (CrucibleState p _ _ sL) = p == end && sL >= 4
+    (_, end) = bounds cityMap
+    newOptions = mkNewOptions cityMap ultraCrucibleTurningRules
+    ultraCrucibleTurningRules n (left, forward, right)
+      | n < 4 = [forward]
+      | n >= 10 = [left, right]
+      | otherwise = [left, forward, right]
+
+aStar :: (DayState -> Bool) -> (DayState -> [DayState]) -> [DayState] -> Maybe Int
+aStar isFinished newOptions starts = go (foldr insert M.empty starts) M.empty
+  where
+    go (M.minView -> Just (current@(CrucibleState _ hL _ _), rest)) visited
+      | isFinished current = pure hL
       | isJust $ mfilter (<= hL) $ visited M.!? toKey current = go rest visited
       | otherwise =
-          let priorityQueue' = foldr insert rest (options current)
+          let priorityQueue' = foldr insert rest (newOptions current)
            in go priorityQueue' (M.insert (toKey current) hL visited)
     go _ _ = empty
-    toKey (AStarState p _ d sl) = (p, d, sl)
-    insert = liftA2 M.insert (\(AStarState p hl d sl) -> (hl, p, d, sl)) id
-    start =
-      AStarState
-        { position = startPos,
+    toKey (CrucibleState p _ d sl) = (p, d, sl)
+    insert = liftA2 M.insert (\(CrucibleState p hl d sl) -> (hl, p, d, sl)) id
+
+mkStart :: Array Pos Int -> [DayState]
+mkStart cityMap = [down, down {direction = R}]
+  where
+    down =
+      CrucibleState
+        { position = start,
           heatLoss = 0,
           direction = D,
           straightLine = 0
         }
-    cityLimits@(startPos, end) = bounds cityMap
-    options (AStarState pos loss dir straight) =
-      filter (inRange cityLimits . position) $ case straight of
-        n
-          | n < 4 -> [forward]
-          | n >= 10 -> [left, right]
-          | otherwise -> [left, right, forward]
-      where
-        forward = newState dir straight
-        newState d sl =
-          let pos' = next pos d
-           in AStarState pos' (loss + (cityMap ! pos')) d (sl + 1)
-        (left, right) = case dir of
-          L -> (newState D 0, newState U 0)
-          R -> (newState U 0, newState D 0)
-          U -> (newState L 0, newState R 0)
-          D -> (newState R 0, newState L 0)
+    (start, _) = bounds cityMap
+
+mkNewOptions :: Array Pos Int -> TurningRules -> DayState -> [DayState]
+mkNewOptions cityMap f (CrucibleState pos loss dir straight) =
+  filter (inRange cityLimits . position) $ f straight (left, forward, right)
+  where
+    cityLimits = bounds cityMap
+    forward = newState dir straight
+    newState d sl =
+      let pos' = next pos d
+       in CrucibleState pos' (loss + (cityMap ! pos')) d (sl + 1)
+    (left, right) = case dir of
+      L -> (newState D 0, newState U 0)
+      R -> (newState U 0, newState D 0)
+      U -> (newState L 0, newState R 0)
+      D -> (newState R 0, newState L 0)
 
 next :: Pos -> Direction -> Pos
 next (r, c) L = (r, c - 1)
