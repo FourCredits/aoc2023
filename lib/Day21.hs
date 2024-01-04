1363020bb7af8df56cd5c216965152cb6006ea92
Erik Grundy <erik.grundy@gmail.com>
2024-01-04 18:28:42 +1300

    day 21 part 2
    not so easy peesy

--- lib/Day21.hs
+++ lib/Day21.hs
@@ -1,25 +1,63 @@
-module Day21 (parse, part1) where
+{-# LANGUAGE TupleSections #-}
 
-import Data.List ((!!))
+module Day21 (parse, part1, part2, solve) where
+
+import Data.List (maximum, (!!))
 import qualified Data.Set as S
 
 type Pos = (Int, Int)
 
-parse :: Text -> Maybe (Pos, Set Pos)
-parse input = case (positionsOf 'S' &&& positionsOf '#') $ positionWise input of
-  ([startPos], rocks) -> Just (startPos, S.fromList rocks)
-  _ -> Nothing
+solve :: Text -> (Text, Text)
+solve = (display (part1 64) &&& display part2) . parse
   where
-    positionsOf char positions = map fst $ filter ((== char) . snd) positions
+    display f = maybe "parse error" (show . f)
+
+parse :: Text -> Maybe (Pos, Set Pos, Pos)
+parse input = positionsOf 'S' positions & viaNonEmpty head <&> (,rocks,bounds)
+  where
+    rocks = S.fromList $ positionsOf '#' positions
+    bounds = (maxR + 1, maxC + 1)
+    maxR = maximum $ map (fst . fst) positions
+    maxC = maximum $ map (snd . fst) positions
+    positions = positionWise input
+    positionsOf char = map fst . filter ((== char) . snd)
     positionWise board = do
       (row, line) <- zip [0 ..] $ lines board
       (column, char) <- zip [0 ..] $ toString line
       pure ((row, column), char)
 
-part1 :: Int -> (Pos, Set Pos) -> Int
-part1 steps (start, rocks) = S.size (iterate nextStep (one start) !! steps)
+part1 :: Int -> (Pos, Set Pos, Pos) -> Int
+part1 steps (start, rocks, _) = S.size (iterate nextStep (one start) !! steps)
   where
-    nextStep acc = (`S.difference` rocks) $ S.unions $ S.map neighbours acc
+    nextStep = (S.\\ rocks) . S.unions . S.map neighbours
 
 neighbours :: Pos -> Set Pos
 neighbours (r, c) = S.fromList [(r, c + 1), (r, c - 1), (r - 1, c), (r + 1, c)]
+
+-- uses this solution as inspiration:
+-- https://github.com/derailed-dash/Advent-of-Code/blob/master/src/AoC_2023/Dazbo's_Advent_of_Code_2023.ipynb
+-- in short, the area that you can reach when there are no rocks is a diamond.
+-- so it's area would just be x * x, where x is the number of steps you take,
+-- divided by 2 (basically because of how a manhattan grid works).
+-- however, the rocks complicate this, by providing obstacles. that said,
+-- the rocks can't introduce high-order elements complicating the area, so we
+-- know it's a quadratic function. all we need to do, then, is determine the
+-- coefficients of that function, which we do by finding three points on that
+-- curve.
+part2 :: (Pos, Set Pos, Pos) -> Int
+part2 (start, rocks, (maxR, maxC)) = a * (x * x) + b * x + c
+  where
+    points = map (S.size . snd3) $ iterate nextStep (mempty, mempty, one start)
+    (p, q, r) = (points !! 65, points !! (65 + 131), points !! (65 + 131 + 131))
+    a = q - p - b
+    b = (4 * q - 3 * p - r) `div` 2
+    c = p
+    s = 26501365
+    x = (s - 65) `div` 131
+    snd3 (_, y, _) = y
+    nextStep (prev, current, frontier) = (current, current', frontier')
+      where
+        frontier' = frontier & S.map neighbours & S.unions & filterOut
+        filterOut = (S.\\ prev) . S.filter isNotRock
+        current' = S.union prev frontier'
+    isNotRock (row, col) = (row `mod` maxR, col `mod` maxC) `S.notMember` rocks
--- test/Day21Spec.hs
+++ test/Day21Spec.hs
@@ -6,12 +6,16 @@ import Utils (realInput)
 
 spec :: Spec
 spec = do
+  -- no example for part 2, because is uses assumptions from the real input that
+  -- aren't true in the example input
   context "examples" $ do
-    it "part 1" $ part1 6 <$> parse exampleInput `shouldBe` Just 16
+    let input = parse exampleInput
+    it "part 1" $ part1 6 <$> input `shouldBe` Just 16
 
   context "real" $ do
     real <- realInput 21 & runIO <&> parse
     it "part 1" $ part1 64 <$> real `shouldBe` Just 3646
+    it "part 2" $ part2 <$> real `shouldBe` Just 606188414811259
 
 exampleInput :: Text
 exampleInput =
